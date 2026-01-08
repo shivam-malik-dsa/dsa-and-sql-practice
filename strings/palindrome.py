7a043f0ea0c56275900d55a343b9b8a8340a0253
Shivam Malik <shivammalik.work@gmail.com>
2026-01-08 18:48:41 +0530

    Added palindrome check using two pointer approach

---
+++ strings/palindrome.py
@@ -0,0 +1,15 @@
+def is_palindrome(s):
+    left, right = 0, len(s) - 1
+
+    while left < right:
+        if s[left] != s[right]:
+            return False
+        left += 1
+        right -= 1
+
+    return True
+
+
+if __name__ == "__main__":
+    s = "madam"
+    print("Is Palindrome:", is_palindrome(s))
