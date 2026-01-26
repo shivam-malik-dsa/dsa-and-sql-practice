b223b1c058c2100f3346c49f67c8146b6c44ee4a
Shivam Malik <shivammalik.work@gmail.com>
2026-01-26 13:01:26 +0530

    Added find first occurrence function

---
+++ searching/first_occurrence.py
@@ -0,0 +1,19 @@
+def first_occurrances(arr,target):
+    left = 0
+    right = len(arr)-1
+    result = -1
+
+    while left <= right:
+        mid = (left+right)//2
+
+        if arr[mid] == target:
+            result = mid
+            right = mid-1
+        
+        elif arr[mid] < target:
+            left = mid+1
+
+        else:
+            right = mid-1
+
+    return result 
