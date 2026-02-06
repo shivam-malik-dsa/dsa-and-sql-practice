1a4fed5799cfb30f38bdab1f3d8e20ea9be3397a
Shivam Malik <shivammalik.work@gmail.com>
2026-02-06 14:59:23 +0530

    Implement insert at beginning in linked list

---
+++ python-dsa/linked list/insert_at_begining.py
@@ -0,0 +1,20 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+class linked_list:
+    def __init__(self):
+        self.head = None
+
+    def insert_at_begining(self,data):
+        new = Node(data)
+        new.next = self.head
+        self.head = new
+
+    def printll(self):
+        temp = self.head
+        while temp:
+            print(temp.data, end=" ")
+            temp = temp.next
+        print("None")        
