b67d5b531fe88ab9cf4bae28a2a849b6518d8af3
Shivam Malik <shivammalik.work@gmail.com>
2026-02-06 15:00:08 +0530

    Implement insert at position in linked list

---
+++ python-dsa/linked list/insert_at_position.py
@@ -0,0 +1,36 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+class linked_list:
+    def __init__(self):
+        self.head = None
+
+    def insert_at_position(self,data,pos):
+        new=Node(data)
+
+        if pos == 0:
+            new.next = self.head
+            self.head = new
+            return
+    
+        temp = self.head
+        i = 0
+        while temp is not None and i < pos-1:
+            temp = temp.next
+            i += 1
+    
+        if temp is None:
+            print("out of range")
+            return
+    
+        new.next = temp.next
+        temp.next = new    
+
+    def printll(self):
+        temp = self.head
+        while temp:
+            print(temp.data, end=" ")
+            temp = temp.next
+        print("none")    
